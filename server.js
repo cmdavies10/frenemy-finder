9dd06419e26179aaff99b5b4a0e41b80d6b72cfa
Chris Davies <c.m.davies10@gmail.com>
2019-10-30 16:56:31 -0400

    html pages served, compatibility logic complete

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // insert routes
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function() {
     console.log(`Listening on PORT ${PORT}`);
