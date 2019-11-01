6bda8b28ad4097b4973a4f95289dd7c543bac07e
Chris Davies <c.m.davies10@gmail.com>
2019-10-31 20:26:54 -0400

    frenemy data updated, survey fleshed out

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,14 +1,14 @@
 var friends = [{
         "name": "Captain Hook",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo": "https://upload.wikimedia.org/wikipedia/en/thumb/1/1b/CaptainHookcartoon.jpg/250px-CaptainHookcartoon.jpg",
         "scores": [
             "5",
-            "1",
+            "5",
             "4",
             "4",
             "5",
-            "1",
-            "2",
+            "4",
+            "4",
             "5",
             "4",
             "1"
@@ -16,7 +16,7 @@ var friends = [{
     },
     {
         "name": "Darth Vader",
-        "photo": "",
+        "photo": "https://images.halloweencostumes.com/products/10008/2-1-99300/authentic-darth-vader-costume-alt14.jpg",
         "scores": [
             "4",
             "2",
@@ -32,7 +32,7 @@ var friends = [{
     },
     {
         "name": "Michael Myers",
-        "photo": "",
+        "photo": "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/aa68-d023-01113-r-1539890493.jpg?crop=1.00xw:0.668xh;0,0.215xh&resize=480:*",
         "scores": [
             "1",
             "1",
@@ -46,6 +46,38 @@ var friends = [{
             "1"
         ]
     },
+    {
+        "name": "Voldemort",
+        "photo": "https://bookriot.com/wp-content/uploads/2019/10/voldemort-feature-640x340.jpg",
+        "scores": [
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1"
+        ]
+    },
+    {
+        "name": "Freddy Krueger",
+        "photo": "https://upload.wikimedia.org/wikipedia/en/thumb/e/eb/Freddy_Krueger_%28Robert_Englund%29.jpg/220px-Freddy_Krueger_%28Robert_Englund%29.jpg",
+        "scores": [
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1"
+        ]
+    }
 
 ];
 
